afae6df14be177f59fde1d0f5dfe7fee2ebfb514
naka-nana <harunanapanda@icloud.com>
2024-12-02 16:57:20 +0900

    ActiveHash導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,10 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :item_info, :region_id, :category_id, :condition_id, :shipping_id, :shipping_day_id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  belongs_to :region
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping
+  belongs_to :shipping_day
 end
